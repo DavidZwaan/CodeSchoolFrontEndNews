79a5712405173d083b880934c5e6d72633b7fd29
Raf Smits <raf_smits@hotmail.com>
2016-07-05 16:57:54 +0200

    Add Modal functions

--- app.js
+++ app.js
@@ -14,4 +14,16 @@ $(document).ready(function(){
 	
 	$('.js-form').toggleClass('is-visible');
   });
+  
+  $('.js-show-modal').on('click', function(e){
+	e.preventDefault();
+	
+	$('.js-modal-overlay').addClass('is-visible');
+	$('.js-modal').addClass('is-visible');
+  });
+  
+  $('.js-modal-overlay').on('click', function(){
+	$('.js-modal-overlay').removeClass('is-visible');
+	$('.js-modal').removeClass('is-visible');  
+  });
 });
